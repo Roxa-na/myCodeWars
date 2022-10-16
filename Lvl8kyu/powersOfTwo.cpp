5523df48f8d9aa96240fe9e838273239e3e863d6
Roxana <93798851+Roxa-na@users.noreply.github.com>
2022-10-16 15:06:30 +0200

    Create powersOfTwo.cpp

---
+++ Lvl8kyu/powersOfTwo.cpp
@@ -0,0 +1,17 @@
+//Complete the function that takes a non-negative
+//integer n as input, and returns a list of
+//all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+#include <vector>
+#include <cstdint>
+#include <cmath>
+using namespace std;
+
+std::vector<uint64_t> powers_of_two(int n) {
+  vector<uint64_t> result;
+  for (int i = 0; i <= n; i++)
+    {
+    result.push_back(pow(2,i));
+  }
+  return result;
+}
