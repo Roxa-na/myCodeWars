b12b7335161caf56adcd6d29e9119bdfb68eccaa
Roxana <93798851+Roxa-na@users.noreply.github.com>
2022-10-17 20:45:42 +0200

    Update centuryFromYear.cpp

--- Lvl8kyu/centuryFromYear.cpp
+++ Lvl8kyu/centuryFromYear.cpp
@@ -21,3 +21,11 @@ int centuryFromYear(int year)
   if (year % 100 == 0) return year/100;
   else return year/100 + 1 ;
 }
+// better solution
+#include <string>
+#include <iostream>
+using namespace std;
+int centuryFromYear(int year)
+{
+return (year + 99) / 100;
+}
