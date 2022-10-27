15a7e7c4b60038a9bda6cc68dfce7666505fdf92
Roxana <93798851+Roxa-na@users.noreply.github.com>
2022-10-27 14:13:51 +0200

    Create You_only_need_one.cpp

---
+++ Lvl8kyu/You_only_need_one.cpp
@@ -0,0 +1,17 @@
+// You will be given an array a and a value x. All you need to do is
+//check whether the provided array contains the value.
+//
+// Array can contain numbers or strings. X can be either.
+//
+// Return true if the array contains the value, false if not.
+// my sol
+#include <vector>
+#include <string>
+
+bool check(const std::vector<std::string>& seq, const std::string& elem) {
+  for (auto& el : seq)
+    {
+    if (el == elem) return true;
+  }
+    return false;
+}
