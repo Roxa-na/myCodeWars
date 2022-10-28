57cc0620e2946af99ae552eddb365640ac6a9235
Roxana <93798851+Roxa-na@users.noreply.github.com>
2022-10-28 09:05:23 +0200

    Create getNthNumber.cpp

---
+++ Lvl8kyu/getNthNumber.cpp
@@ -0,0 +1,10 @@
+// Return the Nth Even Number
+//
+// Example(Input --> Output)
+//
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+int nthEven(int n) {
+  // your code here
+  return 2 * (n-1);
+}
