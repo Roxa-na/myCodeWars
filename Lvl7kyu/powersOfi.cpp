ab8d40a197094383c085ae8846acf106f566b8bf
Roxana <93798851+Roxa-na@users.noreply.github.com>
2022-12-25 14:38:29 +0100

    Create powersOfi.cpp

---
+++ Lvl7kyu/powersOfi.cpp
@@ -0,0 +1,19 @@
+// i is the imaginary unit, it is defined by i²=−1i² = -1i²=−1, therefore it is a solution to x²+1=0x² + 1 = 0x²+1=0.
+// Your Task
+
+// Complete the function pofi that returns iii to the 
+//power of a given non-negative integer in its simplest form,
+// as a string (answer may contain iii).
+// my sol
+std::string pofi(unsigned n) {
+  if (n%4 == 0) return "1";
+  else if (n%4 == 1) return "i";
+  else if (n%4 == 2) return "-1";
+  else if (n%4 == 3) return "-i";
+}
+// other sol
+#include <string>
+
+std::string pofi(unsigned n) {
+  return (std::string[]){"1", "i", "-1", "-i"}[n % 4];
+}
