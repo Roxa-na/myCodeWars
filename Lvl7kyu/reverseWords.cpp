9837cf22b9e8debad7df97e0890d042ed5db8c5e
Roxana <93798851+Roxa-na@users.noreply.github.com>
2022-12-27 17:44:00 +0100

    Create reverseWords.cpp

---
+++ Lvl7kyu/reverseWords.cpp
@@ -0,0 +1,22 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// Examples
+
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+// my sol
+#include <algorithm>
+using namespace std;
+std::string reverse_words(std::string str)
+{
+  unsigned int pos = 0;
+  for (unsigned int i = 0; i < str.size(); i++)
+    {
+    if (str[i] == ' ')
+      {
+      reverse(str.begin() + pos,str.begin() + i);
+      pos = i+1;
+    } else if (i == str.size() -1) reverse(str.begin() + pos,str.begin() + i+1);
+  }
+  return str;
+}
