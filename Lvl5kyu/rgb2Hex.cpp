9ae3b475537dc58227fed86e4762cc5ac6660691
Roxana <93798851+Roxa-na@users.noreply.github.com>
2023-08-25 13:47:19 +0200

    Update rgb2Hex.cpp

--- Lvl5kyu/rgb2Hex.cpp
+++ Lvl5kyu/rgb2Hex.cpp
@@ -20,3 +20,23 @@ class RGBToHex
     return s;
   }
 };
+// another sol // better
+
+#include <sstream>
+#include <iomanip>
+
+class RGBToHex
+{
+  public:
+  static std::string rgb(int r, int g, int b);
+};
+
+std::string RGBToHex::rgb(int r, int g, int b)
+{
+  std::ostringstream oss;
+  oss << std::uppercase << std::setfill('0') << std::hex
+      << std::setw(2) << (r < 0 ? 0 : r > 255 ? 255 : r)
+      << std::setw(2) << (g < 0 ? 0 : g > 255 ? 255 : g)
+      << std::setw(2) << (b < 0 ? 0 : b > 255 ? 255 : b);
+  return oss.str();
+}
